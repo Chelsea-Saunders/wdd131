f65bef8960c50cee05b9a44d4cdf3ba9364373af
chelsea-saundersgit config --global user.email sau24003@byui.edu <168315598+Chelsea-Saunders@users.noreply.github.com>
2024-11-11 11:06:20 -0700

    added form id

---
+++ week7/creditcard/creditcard.js
@@ -0,0 +1,18 @@
+// function submitForms() {
+//     const frontForm = document.getElementById('card-front-form');
+//     const backForm = document.getElementById('card-back-form');
+
+//     // Check if the front form is valid
+//     if (!frontForm.reportValidity()) {
+//         alert("Please complete the card front form correctly.");
+//         return;
+//     }
+
+//     // Check if the back form is valid
+//     if (!backForm.reportValidity()) {
+//         alert("Please complete the card back from correctly.");
+//     }
+
+//     frontForm.submit();
+//     backForm.stubmit();
+// }
