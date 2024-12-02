80123678f9342deef0160007abf647374224b06f
chelsea-saundersgit config --global user.email sau24003@byui.edu <168315598+Chelsea-Saunders@users.noreply.github.com>
2024-12-02 08:40:49 -0700

    adding desert rain soap link to main html

---
+++ finale/soap-calc/recipes.mjs
@@ -0,0 +1,36 @@
+const recipes = [
+	{
+		author: 'Provo High Culinary Students',
+		url: '',
+		isBasedOn: '',
+        experienceLevel: 'Beginner',
+		datePublished: '2024-11-26',
+		tags: ['Soap', 'Lye', 'Sodium Hydroxide', 'Lard', 'Tallow', 'Coconut', 'Castor'],
+		description: '75% Lard Soap that is super easy to make and great for dry skin.',
+		image: './image/sweet-potato-waffle-md.jpg',
+		recipeIngredientPound: [
+			'.2 lbs Castor Oil',
+			'.3 lbs Coconut Oil',
+			'1.5 lbs Lard',
+            '.54 lbs Water',
+            '.27 lbs Sodium Hydroxide',
+            'fragrance or essential oil (optional)',
+		],
+        recipeIngredientsOunce: [
+            '3.2 oz Castor Oil',
+            '4.8 oz Coconut Oil',
+            '24 oz Lard',
+            '8.7 oz Water',
+            '4.35 oz Sodium Hydroxide',
+            'fragrance or essential oil (optional)',
+        ],
+        recipeIngredientsGrams: [
+            '90.72 g Castor Oil',
+            '136.08 g Coconut Oil',
+            '680.39 g Lard',
+            '246.7 g Water',
+            '123.35 g Sodium Hydroxide',
+            'fragrance or essential oil (optional)',
+        ]
+    }
+];
